25b804536d04b2b2501c8bfa2b157791819d53fe
JohnDavidCabal <Johndavidcabal000@gmail.com>
2022-04-26 15:25:43 +0800

    Finish the Encryption of the Message

--- Main.java
+++ Main.java
@@ -1,11 +1,6 @@
-import org.w3c.dom.stylesheets.LinkStyle;
-
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        PolybiusEncryption polybiusEncryption = new PolybiusEncryption("LIFE", "john");
-        polybiusEncryption.PolybiusEncryption_2();
+        PolybiusEncryption polybiusEncryption = new PolybiusEncryption("", "john");
+        System.out.println(polybiusEncryption.Encryption());
     }
-
 }
--- PolybiusDecryption.java
+++ PolybiusDecryption.java
@@ -3,41 +3,46 @@ import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-public class PolybiusEncryption {
-
+public class PolybiusDecryption {
     private final String message;
     private final String key;
 
-    PolybiusEncryption(String key, String message) {
+    PolybiusDecryption(String key, String message) {
         this.key = key;
         this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
     }
 
     public String getKey() {
         return key;
     }
 
-    public int Encryption() {
+    public String getMessage() {
+        return  message;
+    }
+
+    public void Decryption() {
+
         // The Final List where we stored the key message and the normal alphabet.
         ArrayList<ArrayList<Character>> alphabetWithKeyMessage = new ArrayList<>();
         // Here we permanently put the Characters of the key message.
         ArrayList<Character>  userMessageCharacterList = new ArrayList<>();
         // Here we put the Key Message and the normal alphabet together to remove duplicates.
-        Set<Character> temp2 = new LinkedHashSet<>();
+        Set<Character> temp2 = new LinkedHashSet<Character>();
         // Just the Blueprint of a normal alphabet.
         ArrayList<Character> normalAlphabet = new ArrayList<>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
                 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'));
 
+        /*String userMessageKey = "Marvel";
+        System.out.println("User Inputted Key : " + userMessageKey);
+*/
         // Adding the key Message to Array List.
         int counter = 0;
         for(int i = 0; i < getKey().length(); i++) {
             userMessageCharacterList.add(i, getKey().toUpperCase().charAt(counter));
             counter = counter + 1;
         }
+
+        //System.out.println("User Message Character : " + userMessageCharacterList);
 
         // adding the User Message and the Normal Alphabet letters.
         temp2.addAll(userMessageCharacterList);
@@ -46,36 +51,13 @@ public class PolybiusEncryption {
         // convert the LinkedHashSet to ArrayList.
         ArrayList<Character> list = new ArrayList<>(temp2);
 
-        int listCounter = 0;
+        int messageCounter = 0;
         for(int rows = 0; rows < 5; rows++) {
-            alphabetWithKeyMessage.add(rows, new ArrayList<>());
+            alphabetWithKeyMessage.add(rows, new ArrayList<Character>());
             for (int cols = 0; cols < 5; cols++) {
-                alphabetWithKeyMessage.get(rows).add(cols, list.get(listCounter));
-                listCounter = listCounter + 1;
+                alphabetWithKeyMessage.get(rows).add(cols, list.get(messageCounter));
+                messageCounter = messageCounter + 1;
             }
         }
-
-        // Printing 2D ArrayList.
-        for (ArrayList<Character> temp: alphabetWithKeyMessage) {
-            System.out.println(temp);
-        }
-
-        ArrayList<Integer> encryptedMessage = new ArrayList<>();
-        for(int messageCounter = 0; messageCounter < getMessage().length(); messageCounter++) {
-            for (int rows = 0; rows < 5; rows++) {
-                for (int cols = 0; cols < 5; cols++) {
-                    if (alphabetWithKeyMessage.get(rows).get(cols).equals(getMessage().toUpperCase().charAt(messageCounter)) && messageCounter < getMessage().length()) {
-                        encryptedMessage.add(rows);
-                        encryptedMessage.add(cols);
-                    }
-                }
-            }
-        }
-
-        int result = 0;
-        for (Integer integer : encryptedMessage) {
-            result = result * 10 + integer;
-        }
-        return result;
     }
 }
