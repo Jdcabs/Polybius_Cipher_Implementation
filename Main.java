b8d381e5b7d68af41d6af284c412bdffc667aff1
JohnDavidCabal <Johndavidcabal000@gmail.com>
2022-04-26 10:11:34 +0800

    added some Modification

--- Main.java
+++ Main.java
@@ -1,36 +1,11 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        create2DArrayList();
+        PolybiusEncryption polybiusEncryption = new PolybiusEncryption("LIFE", "john");
+        polybiusEncryption.PolybiusEncryption_2();
     }
-    static void create2DArrayList(){
-        ArrayList<ArrayList<Character>> Alphabet = new ArrayList<>();
-        // Here we're Instantiating the Row0 of the ArrayList,
-        Alphabet.add(new ArrayList<Character>(Arrays.asList('A','B','C','D','E')));
-        Alphabet.add(new ArrayList<Character>(Arrays.asList('F','G','H','I','J')));
-        Alphabet.add(new ArrayList<Character>(Arrays.asList('K','L','M','N','O')));
-        Alphabet.add(new ArrayList<Character>(Arrays.asList('P','Q','R','S','T')));
-        Alphabet.add(new ArrayList<Character>(Arrays.asList('U','V','W','X','Y')));
 
-        String userMessage = "david";
-
-        /*Alphabet.get(0).remove(0);
-        Alphabet.get(0).add(0,'d');*/
-
-        // int col = 0;
-        /*int alphabetSize = Alphabet.size();
-        for (int row = 0; row < alphabetSize; row++) {
-            for(int cols = 0; cols < alphabetSize; cols++){
-                System.out.println(Alphabet.get(row).get(cols));
-            }
-        }*/
-
-        for (ArrayList<Character> alph : Alphabet) {
-            System.out.println(alph);
-        }
-    }
 }
--- PolybiusEncryption.java
+++ PolybiusEncryption.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class PolybiusEncryption {
 
@@ -19,15 +21,50 @@ public class PolybiusEncryption {
         return key;
     }
 
-    public void PolybiusEnryption() {
-        // Here we create a Multidimensional Arraylist.
-        ArrayList<ArrayList<Character>> Alphabet = new ArrayList<>();
-        // Here we're Instantiating the Row0 to Row5 of the ArrayList, at the same time we add the Alphabetical table.
-        Alphabet.add(new ArrayList<Character>(Arrays.asList('A','B','C','D','E')));
-        Alphabet.add(new ArrayList<Character>(Arrays.asList('F','G','H','I','J')));
-        Alphabet.add(new ArrayList<Character>(Arrays.asList('K','L','M','N','O')));
-        Alphabet.add(new ArrayList<Character>(Arrays.asList('P','Q','R','S','T')));
-        Alphabet.add(new ArrayList<Character>(Arrays.asList('U','V','W','X','Y')));
+    public void PolybiusEncryption_2() {
+        // The Final List where we stored the key message and the normal alphabet.
+        ArrayList<ArrayList<Character>> alphabetWithKeyMessage = new ArrayList<>();
+        // Here we permanently put the Characters of the key message.
+        ArrayList<Character>  userMessageCharacterList = new ArrayList<>();
+        // Here we put the Key Message and the normal alphabet together to remove duplicates.
+        Set<Character> temp2 = new LinkedHashSet<Character>();
+        // Just the Blueprint of a normal alphabet.
+        ArrayList<Character> normalAlphabet = new ArrayList<>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'));
 
+        String userMessageKey = "Marvel";
+        System.out.println("User Inputted Key : " + userMessageKey);
+
+        // Adding the key Message to Array List.
+        int counter = 0;
+        for(int i = 0; i < getKey().length(); i++) {
+            userMessageCharacterList.add(i, getKey().toUpperCase().charAt(counter));
+            counter = counter + 1;
+        }
+        System.out.println("User Message Character : " + userMessageCharacterList);
+
+        // adding the User Message and the Normal Alphabet letters.
+        for(int i = 0; i < userMessageCharacterList.size(); i++) {
+            temp2.add(userMessageCharacterList.get(i));
+        }
+        for(int i = 0; i < normalAlphabet.size(); i++) {
+            temp2.add(normalAlphabet.get(i));
+        }
+
+        // convert the LinkedHashSet to ArrayList.
+        ArrayList<Character> list = new ArrayList<>(temp2);
+
+        int messageCounter = 0;
+        for(int rows = 0; rows < 5; rows++) {
+            alphabetWithKeyMessage.add(rows, new ArrayList<Character>());
+            for (int cols = 0; cols < 5; cols++) {
+                alphabetWithKeyMessage.get(rows).add(cols, list.get(messageCounter));
+                messageCounter = messageCounter + 1;
+            }
+        }
+        // Printing 2D ArrayList.
+        for (ArrayList<Character> temp: alphabetWithKeyMessage) {
+            System.out.println(temp);
+        }
     }
 }
