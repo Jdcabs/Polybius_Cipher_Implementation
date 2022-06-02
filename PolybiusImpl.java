d034c72e767ffce2bc8e46547a689178bd31c0cc
John David S Cabal <johndavidcabal000@gmail.com>
2022-06-02 09:05:11 +0800

    Minor Changes
    Removing Unused variables and make some changes on how the program runs.

--- PolybiusImpl.java
+++ PolybiusImpl.java
@@ -1,86 +1,27 @@
 import java.util.Scanner;
 
 public class PolybiusImpl {
-
-    private String key;
-    private String userMessage;
-    private String encryptedMessage;
-    private String DecryptedMessage;
-
-    private PolybiusEncryption polybiusEncryption;
-
-    private  PolybiusDecryption polybiusDecryption;
-
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
-    }
-
-    public String getUserMessage() {
-        return userMessage;
-    }
-
-    public void setUserMessage(String userMessage) {
-        this.userMessage = userMessage;
-    }
-
-    public String getEncryptedMessage() {
-        return encryptedMessage;
-    }
-
-    public void setEncryptedMessage(String encryptedMessage) {
-        this.encryptedMessage = encryptedMessage;
-    }
-
-    public String getDecryptedMessage() {
-        return DecryptedMessage;
-    }
-
-    public void setDecryptedMessage(String decryptedMessage) {
-        DecryptedMessage = decryptedMessage;
-    }
+    private String decision;
 
     public void Begin() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Polybius Message Encryption and Decryption\n");
 
         while(true) {
-            try {
-                System.out.print("Enter a Message to Encrypt: ");
-                setUserMessage(scanner.nextLine());
-
-                System.out.print("Enter a Secret Key: ");
-                setKey(scanner.nextLine());
-
-            }catch (Exception e) {
-                System.out.println("Invalid data! Cause: " + e.getMessage());
-                continue;
-            }
-
+            System.out.print("Press [E] to Encrypt [D] to Decrypt: ");
             while(true) {
                 try {
-                    System.out.print("\nPress [E] to Encrypt [D] to Decrypt: ");
-                    final String decision = scanner.nextLine().toString();
+                    decision = scanner.nextLine();
 
                     if(decision.trim().equalsIgnoreCase("E")) {
-                        polybiusEncryption = new PolybiusEncryption(getKey(),getUserMessage());
-                        setEncryptedMessage(polybiusEncryption.Encryption());
-                        System.out.println("Encrypted Message: " + getEncryptedMessage());
-
+                        PolybiusEncryption polybiusEncryption = new PolybiusEncryption();
+                        polybiusEncryption.EncryptionImpl();
+                        System.out.println("Encrypted Message: " + polybiusEncryption.Encryption());
+                        break;
                     }else if(decision.trim().equalsIgnoreCase("D")) {
-
-                        System.out.print("\nPlease Enter the Encrypted Message: ");
-
-                        setDecryptedMessage(scanner.nextLine());
-                        final Long decryption = Long.parseLong(getDecryptedMessage());
-
-                        polybiusDecryption = new PolybiusDecryption(getKey(), decryption);
-
+                        PolybiusDecryption polybiusDecryption = new PolybiusDecryption();
+                        polybiusDecryption.DecryptionImpl();
                         System.out.println("Decrypted Message: " + polybiusDecryption.Decryption());
-
                         break;
                     }
                 }catch (Exception e) {
@@ -89,7 +30,7 @@ public class PolybiusImpl {
                 }
             }
 
-            System.out.print("Do you want to try another one? [Y/N]");
+            System.out.print("\nDo you want to try another one? [Y/N]");
             final String yesOrNo = scanner.next();
 
             if(yesOrNo.trim().equalsIgnoreCase("Y")) {
